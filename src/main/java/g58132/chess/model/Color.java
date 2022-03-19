b77f94d2bc8be2c5bd90ba20ea8322f2cb461fe8
58132@etu.he2b.be <58132@etu.he2b.be>
2022-03-19 22:36:10 +0100

    Créer l'énumération Color

---
+++ src/main/java/g58132/chess/model/Color.java
@@ -0,0 +1,25 @@
+package g58132.chess.model;
+
+/**
+ *
+ * Represent the color of a player, of a square, or of a piece of the chess
+ * game.
+ *
+ * @author elhar
+ */
+public enum Color {
+    WHITE, BLACK,;
+
+    /**
+     * Color opposite
+     *
+     * Allows to give the opposite color 
+     *
+     * @return black if the current color is white and white if the current is
+     * black
+     */
+    public Color opposite() {
+        return (this == WHITE) ? BLACK : WHITE;
+    }
+
+}
